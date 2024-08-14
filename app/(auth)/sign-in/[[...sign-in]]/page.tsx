fe38d0961a6122ed6fb4f3b66f7d0a7620711ac6
Deeptanu Bhatta <deeptanubhatta2002@gmail.com>
2024-08-14 21:32:27 +0530

    first commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
+
+
+const page = () => {
+  return (
+      <SignIn/>
+  )
+}
+
+export default page
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs"
+
+
+const page = () => {
+  return (
+          <SignUp/>
+  )
+}
+
+export default page
